a59bb19366afdfaaacd0e345b01b175e4a84a67f
harshal135patil <113527841+harshal135patil@users.noreply.github.com>
2022-12-02 13:59:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import RegistrationForm from './components/index'
+
+const App = () => {
+  return (
+    <div className="App">
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default App;
